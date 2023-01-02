442c9284f031077efa8542cfb325fc9850e1aec6
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-01-01 16:30:28 -0800

    COMMIT #

--- commit_and_push.sh
+++ commit_and_push.sh
@@ -1,5 +1,6 @@
 #add, commit, and push updates. When a separate git repository is updated, a post-commit hook transfers its commit history log to this directory and executes this script.
 
+git log --oneline > $(pwd | rev | cut -f 1 -d / | rev).log
 git add .
 LINE_NUMBER=$(git log --oneline | wc -l | sed s/.*[^1-9]//)
 git commit -m "COMMIT #$LINE_NUMBER"
