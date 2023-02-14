7d86ee5e887343e9ac83b4a6cdb1eac801fe23a3
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-02-14 00:40:06 -0800

    COMMIT #80

--- commit_and_push.sh
+++ commit_and_push.sh
@@ -1,10 +1,12 @@
 #add, commit, and push updates. When a separate git repository is updated, a post-commit hook transfers its commit history log to this directory and executes this script.
 
-ls -1t | grep _project > projects_names_list.tmp
+echo -n > projects_names_list.tmp
 for dir in $(ls -1t | grep _project); do 
-	head -2 ./${dir}/*data | tail -1 | cut -f3 >> projects_dates_list.tmp
+	echo $dir >> projects_names_list.tmp
+	head -2 ./${dir}/*data | tail -1 | cut -f5 >> projects_dates_list.tmp
 	echo $(cat ./${dir}/*log | wc -l) >> projects_commits_list.tmp
 done;
+
 paste -d '|' projects_names_list.tmp projects_dates_list.tmp projects_commits_list.tmp | sed 's/[ ]*|/ | /g' | column -s $' ' -t > projects_summary.tmp
 sort -r -k 3 projects_summary.tmp > projects_summary2.tmp
 mv projects_summary2.tmp projects_summary.tmp
